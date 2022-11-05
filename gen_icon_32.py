29c5054132e61a2293d8085c6f4f4fcc4eb3e1c4
cristminix <cristminix@gmail.com>
2022-11-05 07:28:35 +0700

    ch04

--- gen_icon_32.py
+++ gen_icon_32.py
@@ -1,17 +1,48 @@
 from PIL import Image, ImageDraw, ImageFont
+import sys
+import os
+import getopt
 
-width = 32
-height = 32
+chapter_index = None
+output_dir = None
 
-message = "03"
-img = Image.new("RGB", (width,height), color='gray')
-imgDraw = ImageDraw.Draw(img)
-font = ImageFont.truetype("arialbd.ttf", size=20)
-textWidth, textHeight = imgDraw.textsize(message, font=font)
-xText = (width - textWidth) / 2
-yText = (height - textHeight) / 2
+argv = sys.argv[1:]
 
-# imgDraw.text((xText, yText), message, font=font, fill=(255, 255, 0))
-imgDraw.text((xText, yText),message,font=font, fill=(255,255,255))
-filename = "chapter_%s_32.png" % (message)
-img.save(filename)
+try:
+    opts, args = getopt.getopt(argv, "i:o:", 
+                               ["index =",
+                                "output_dir ="])
+  
+except:
+    print("Error")
+
+for opt, arg in opts:
+    if opt in ['-i', '--index']:
+        chapter_index = arg
+    elif opt in ['-o', '--output_dir']:
+        output_dir = arg
+  
+
+print( chapter_index , output_dir)
+
+if chapter_index != None and output_dir != None:
+	
+	if os.path.exists(output_dir):
+		width = 32
+		height = 32
+
+		message = "%02d" % int(chapter_index)
+		img = Image.new("RGB", (width,height), color='gray')
+		imgDraw = ImageDraw.Draw(img)
+		font = ImageFont.truetype("arialbd.ttf", size=20)
+		textWidth, textHeight = imgDraw.textsize(message, font=font)
+		xText = (width - textWidth) / 2
+		yText = (height - textHeight) / 2
+
+		# imgDraw.text((xText, yText), message, font=font, fill=(255, 255, 0))
+		imgDraw.text((xText, yText),message,font=font, fill=(255,255,255))
+		filename = "%s/chapter_%s_32.png" % (output_dir,message)
+		img.save(filename)
+	else:
+		print("Usage : ./gen_icon_32 <chapter_index> <output_dir>")
+		print("Error : %s directory not exists" % (output_dir))
---
+++ gen_icon_32.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 gen_icon_32.py -i $1 -o $2
